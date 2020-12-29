83d231e31ef645f1d576fbfe02cad864608fbc66
lukkaw <lukasz.kawka@syncron.com>
2020-12-29 19:37:21 +0100

    v2

--- hsv_filtering.py
+++ hsv_filtering.py
@@ -1,6 +1,3 @@
-import cv2
-import numpy as np
-
 from segmentation import *
 
 
@@ -9,8 +6,8 @@ def process1(img):
     hand = cv2.bitwise_and(img, img, mask=mask)
 
     hsv = cv2.cvtColor(hand, cv2.COLOR_BGR2HSV)
-    mask = hsv_filter_mask(hsv, np.array([90, 10, 115]), np.array([200, 132, 255]))
-    mask = mask | hsv_filter_mask(hsv, np.array([0, 10, 100]), np.array([5, 121, 255]))
+    mask = hsv_filter_mask(hsv, (90, 10, 115), (200, 132, 255))
+    mask = mask | hsv_filter_mask(hsv, (0, 10, 100), (5, 121, 255))
 
     mask = blur(mask)
     mask = noise_removal(mask, it=3)
